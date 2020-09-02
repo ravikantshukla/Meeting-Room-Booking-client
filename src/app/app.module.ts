ef14996a3d1354498a9855804b3a3c98b66e430b
= <=>
2020-09-02 09:26:27 +0530

    initial commit for meeting room  booking frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { RoomsComponent } from './admin/rooms/rooms.component';
+import { UsersComponent } from './admin/users/users.component';
+import { Routes, RouterModule } from '@angular/router';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
+const routes: Routes = [
+  { path: 'admin/users', component: UsersComponent },
+  { path: 'admin/rooms', component: RoomsComponent },
+  { path: '', component: CalendarComponent },
+  { path: '404', component: PagenotfoundComponent },
+  { path: '**', redirectTo: '/404' },
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CalendarComponent,
+    RoomsComponent,
+    UsersComponent,
+    PagenotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
